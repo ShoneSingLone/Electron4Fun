b5b2de9ea4ca86b858910220a6a3ce70ea2a4ae2
ShoneSingLone <ShoneSingLone@gmail.com>
2019-02-04 22:18:27 +0800

    add element-ui

--- src/renderer/modules/Lorikeet/LorikeetPage.vue
+++ src/renderer/modules/Lorikeet/LorikeetPage.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="lorikeet-wrapper">
+    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
     <div id="toolbar">
       <div id="current-folder" v-for="(item, index) in homedir" :key="index">{{item.basename}}</div>
     </div>
@@ -22,10 +23,74 @@ export default {
   },
   data() {
     return {
-      homedir: ""
+      homedir: "",
+      data: [
+        {
+          label: "一级 1",
+          children: [
+            {
+              label: "二级 1-1",
+              children: [
+                {
+                  label: "三级 1-1-1"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label: "一级 2",
+          children: [
+            {
+              label: "二级 2-1",
+              children: [
+                {
+                  label: "三级 2-1-1"
+                }
+              ]
+            },
+            {
+              label: "二级 2-2",
+              children: [
+                {
+                  label: "三级 2-2-1"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          label: "一级 3",
+          children: [
+            {
+              label: "二级 3-1",
+              children: [
+                {
+                  label: "三级 3-1-1"
+                }
+              ]
+            },
+            {
+              label: "二级 3-2",
+              children: [
+                {
+                  label: "三级 3-2-1"
+                }
+              ]
+            }
+          ]
+        }
+      ],
+      defaultProps: {
+        children: "children",
+        label: "label"
+      }
     };
   },
   methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    },
     getHomedir() {
       return os.homedir();
     },
